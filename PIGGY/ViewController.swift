8af4fad11f73cc7a0e462cb7062236b1153d6dd9
Kamylle Wilgozz <65574957+Wilgozz@users.noreply.github.com>
2022-05-14 18:40:13 -0300

    Initial Commit

---
+++ PIGGY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PIGGY
+//
+//  Created by Kamylle Wilgozz on 14/05/2022.
+//  Copyright © 2022 Kamylle Wilgozz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
