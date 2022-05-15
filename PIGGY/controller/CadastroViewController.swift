397822e1cdd40cbfa016180e6a1555c4d5446d7e
Kamylle Wilgozz <65574957+Wilgozz@users.noreply.github.com>
2022-05-15 03:53:02 -0300

    botão para tela home, pickerView do campo EntradaSaida, alert para campos não preenchidos

--- PIGGY/controller/CadastroViewController.swift
+++ PIGGY/controller/CadastroViewController.swift
@@ -18,19 +18,50 @@ class CadastroViewController: UIViewController {
     @IBOutlet weak var campoEntradaSaida: UITextField!
     let entradaSaida = ["entrada", "saída"]
     
+    
+    @IBOutlet weak var campoDescricao: UITextField!
+    
+    
+    
+    @IBOutlet weak var campoValor: UITextField!
+    
+    
+    
+    
     var pickerView = UIPickerView()
+    var pickerViewEntradaSaida = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         pickerView.delegate = self
         pickerView.dataSource = self
+        pickerViewEntradaSaida.delegate = self
+        pickerViewEntradaSaida.dataSource = self
+    
         
         campoDeCategoria.inputView = pickerView
+        campoEntradaSaida.inputView = pickerViewEntradaSaida
         
     }
     
-    
+    @IBAction func botaoTelaCadastro(_ sender: Any) {
+        if let valor = campoValor.text, let descricao = campoDescricao.text, let categoria = campoDeCategoria.text, let entradaESaida = campoEntradaSaida.text {
+            
+            if valor.count <= 0 || descricao.count <= 0 || categoria.count <= 0 || entradaESaida.count <= 0{
+                let alert = UIAlertController(title: "Erro", message: "todos os campos devem estar preenchidos!", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.cancel))
+                present(alert, animated: true)
+                return
+            }
+            
+            //essas variaveis devem ser salvas no sqlite
+            print(valor)
+            print(descricao)
+            print(categoria)
+            print(entradaESaida)
+        }
+    }
 }
 
 extension CadastroViewController: UIPickerViewDelegate, UIPickerViewDataSource {
@@ -39,15 +70,27 @@ extension CadastroViewController: UIPickerViewDelegate, UIPickerViewDataSource {
         return 1
     }
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        if pickerView == pickerViewEntradaSaida {
+            return entradaSaida.count
+        }
+        
         return categorias.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == pickerViewEntradaSaida{
+            return entradaSaida[row]
+        }
         return categorias[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        campoDeCategoria.text = categorias[row]
-        campoDeCategoria.resignFirstResponder()
+        if pickerView == pickerViewEntradaSaida{
+            campoEntradaSaida.text = entradaSaida[row]
+            campoEntradaSaida.resignFirstResponder()
+        }else{
+            campoDeCategoria.text = categorias[row]
+            campoDeCategoria.resignFirstResponder()
+        }
     }
 }
